ab8af4a03378d968aef44e25b6ba984b693ca34f
soohyeon <qpsxhf410@outlook.kr>
2025-06-04 14:54:27 +0900

    25-6-3/chapter-4

--- BikeShop/Menu.xaml.cs
+++ BikeShop/Menu.xaml.cs
@@ -27,19 +27,21 @@ namespace BikeShop
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            NavigationService.Navigate(
+                new Uri("/ProductsManagement.xaml", UriKind.Relative));
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button1_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(
                 new Uri("/Contact.xaml", UriKind.Relative));
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private void Button2_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(
                 new Uri("/Discussion.xaml", UriKind.Relative));
         }
+
     }
 }
